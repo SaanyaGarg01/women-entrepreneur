decc48ce79aa0205fe04a318e9bd5250cd918e5f
SaanyaGarg01 <saanyagarg800@gmail.com>
2025-04-01 23:30:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Funding Form Modal
+function openFundingForm() {
+    alert("Funding application feature coming soon!");
+}
+
+// Virtual Business Consultation
+function bookConsultation() {
+    alert("Booking consultation feature coming soon!");
+}
+
+// UPI Payment
+function processPayment() {
+    alert("Payment processing feature coming soon!");
+}
+
+// AI Chatbot
+function openChatbot() {
+    alert("AI Chatbot launching soon!");
+}
